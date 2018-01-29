06d01f8597bc508fdca60d003e2425bd9b264ad6
s021791 <s021791@fe80:0:0:0:b8bd:adff:fe87:e1e7%8>
2018-01-29 13:30:14 -0500

    Change some files in preparation for choosing the pawn changing panel

--- Chess/src/GraphicsPanel.java
+++ Chess/src/GraphicsPanel.java
@@ -43,6 +43,7 @@ public class GraphicsPanel extends JPanel implements MouseListener{
 	private int player;						// current player (1 or 2)
 	private State state; 					// Current game state
 	private boolean lastClickTurnSwitch; 	// True if the last click moved a piece. Used to draw "check" until they click again
+	private boolean choosingThePieceThatWillReplaceThePawnOnceItCrossesToTheOtherSide; //Does the name not say enough?
 	
 	public GraphicsPanel(){
 
@@ -96,6 +97,9 @@ public class GraphicsPanel extends JPanel implements MouseListener{
 		
         this.setFocusable(true);					 // for keylistener
 		this.addMouseListener(this);
+		
+		//Set choosingThePieceThatWillReplaceThePawnOnceItCrossesToTheOtherSide to false
+		choosingThePieceThatWillReplaceThePawnOnceItCrossesToTheOtherSide = false;
 	}
 	
 	
@@ -252,15 +256,18 @@ public class GraphicsPanel extends JPanel implements MouseListener{
 						if(board[0][i] != null 
 						&& board[0][i].getPlayer()==1
 						&& board[0][i] instanceof Pawn)
+							choosingThePieceThatWillReplaceThePawnOnceItCrossesToTheOtherSide = true;
 							board[0][i] = new Queen(1);
 					}
 					for(int i = 0; i < 8; i++) {
 						if(board[7][i] != null 
 						&& board[7][i].getPlayer()==2
 						&& board[7][i] instanceof Pawn)
+							choosingThePieceThatWillReplaceThePawnOnceItCrossesToTheOtherSide = true;
 							board[7][i] = new Queen(2);
 					}
-							
+					
+					choosingThePieceThatWillReplaceThePawnOnceItCrossesToTheOtherSide = false;
 		        
 			        if(isCheckMate(3-player, board)){//Check if game over
 			          state = State.GAMEOVER;
